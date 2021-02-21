cca0330168998e616cc74427439ab09382c50b43
Gavin Leech <g@gleech.org>
2021-02-21 11:32:45 +0000

    abort rabbithole

--- codes/modinv.py
+++ codes/modinv.py
@@ -1,9 +1,7 @@
 import numpy as np
 from math import gcd 
 
-# If detA is coprime to m, then you can be sure that A is invertible modm.
-
-m = 26
+# If det A is coprime to m, then you can be sure that A is invertible modm.
 
 def det(A) :
 	d = np.linalg.det(A)
@@ -18,11 +16,6 @@ def egcd(a, b):
 	else:
 		gcd, x, y = egcd(b % a, a)
 		return (gcd, y - (b//a) * x, x)
-
-
-
-A = np.matrix([[1, 5], 
-			  [3, 4]])
 
 
 def basic_2x2_inv(A) :
@@ -44,9 +37,48 @@ def inverse_mod_2x2(A, m) :
 
 	Ainv = basic_2x2_inv(A)
 	inv = dinv * Ainv
+	
 	return inv % m
 
 
+def inv(A) :
+	return A
 
-print(inverse_mod_2x2(A, m))
+def inverse_mod(A, m) :
+	d = det(A)
+	print(d)
+	assert( coprime(d, m) )
 
+	gcd, dinv, _ = egcd(d, m)
+	if gcd == -1 :
+		dinv *= -1
+	print(dinv)
+
+	Ainv = inv(A)
+	Ainv = dinv * Ainv
+
+	return Ainv % m
+
+A = np.matrix([[1, 5], 
+			  [3, 4]])
+m = 26
+
+Amodinv = inverse_mod_2x2(A, m)
+check = Amodinv * A % m
+I = np.identity(2)
+assert(np.all(check == I))
+
+
+A = np.matrix( [[17, 17, 5], \
+				[21, 18, 21], \
+				[2, 2, 19]] )
+m = 26
+
+print(inverse_mod(A, m))
+
+solution = np.matrix( [[4, 9, 15], \
+						[15, 17, 6], \
+						[24, 0, 17]] )
+
+
+print(egcd(a, b))
